f6917df90b7156600004b2ea65e3b3a41de5fcbc
Deniz Özdemirli <ozdemirlideniz29@gmail.com>
2026-05-01 14:15:16 +0300

    fix typo

--- src/graph/nodes/__init__.py
+++ src/graph/nodes/__init__.py
@@ -4,7 +4,7 @@ from .class_node import ClassNode
 from .commit_node import CommitNode
 from .file_node import FileNode
 from .function_node import FunctionNode
-from .test_snode import TestNode
+from .tests_node import TestNode
 
 __all__ = [
     "ClassNode",
